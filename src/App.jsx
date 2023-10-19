5a2ef8d43c5c266432ad8b289214ec85bb3373c1
juliusz-jakubowski <juliusz.jakubowski@codete.com>
2023-10-19 23:41:03 +0200

    clear App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 const App = () => (
   <div className="app">
-    <header className="app-header">
-      <p className="header">Vite React Starter 💯</p>
-      <p>
-        Vite + React <br/>
-        ESLint + Prettier + Stylelint
-        <br/>
-        Sass
-        <br/>
-        Jest + Testing Library
-      </p>
-    </header>
+    test
   </div>
 );
 
