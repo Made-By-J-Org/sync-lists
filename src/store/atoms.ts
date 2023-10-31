c51475d21052ec203d41c04338fcdec31cd850fd
juliusz-jakubowski <juliusz.jakubowski@codete.com>
2023-10-31 21:33:04 +0100

    WIP store atoms

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,29 +1,65 @@
-import {Atom, atom} from "jotai";
+import {atom, WritableAtom} from "jotai";
 import {loadable} from "jotai/utils"
+// @ts-ignore
+import {v4 as uuidv4} from 'uuid';
 
 import {API_ENDPOINT} from "../constants";
 
 export interface TaskSchema {
+  id: string,
   description: string,
   value: number,
   checked: boolean
 }
 
-export interface ApiSchema {
+export interface TaskGroupSchema {
+  id: string;
   name: string;
   tasks: Array<TaskSchema>
 }
 
-const fetchDataAsyncAtom: Atom<Promise<Array<ApiSchema>>> = atom(async () => {
-  const res = await fetch(API_ENDPOINT)
-  return await res.json()
-})
+export interface TasksSchema extends Array<TaskGroupSchema> {
+  id: string
+}
+
+export const progressAtom = atom(0)
+const calculateProgress = (lists: TasksSchema | never[]): number => {
+  return 10;
+}
+// no mechanism for caching
+export const listAsyncAtom: WritableAtom<Promise<TasksSchema | never[]>, [args: {
+  tasksListId: string,
+  task: TaskSchema
+}], Promise<TasksSchema>> = atom(
+  async () => {
+    const res: Response = await fetch(API_ENDPOINT)
+    const lists: TasksSchema = await res.json();
+    if (lists && lists.length) {
+      // const calculatedProgress = calculateProgress(lists)
+      // set(progressAtom, calculatedProgress)
+      //
+      lists.forEach((item) => {
+        item.id = uuidv4();
+        item.tasks.forEach(task => {
+          task.id = uuidv4()
+          return task;
+        })
+        return item
+      })
+      return lists
+    }
+    return []
+  },
+  async (get, set, args: { tasksListId: string, task: TaskSchema }) => {
+    const lists: TasksSchema = await get(listAsyncAtom) as TasksSchema
+    const listIndex = lists.findIndex(tasksList => tasksList.id === args.tasksListId)
+    const tasksList = lists[listIndex].tasks
+    lists[listIndex].tasks = [...tasksList, args.task]
+    const calculatedProgress = calculateProgress(lists)
+    set(progressAtom, calculatedProgress)
+    return lists;
+  }
+)
 
 // one of jotai mechanism to handle async data
-export const loadableFetchDataAsyncAtom = loadable(fetchDataAsyncAtom)
-
-export const progressAtom = atom((get) => {
-  const data = get(fetchDataAsyncAtom)
-  console.log({data})
-  return data
-})
+export const loadableFetchDataAsyncAtom = loadable(listAsyncAtom)
