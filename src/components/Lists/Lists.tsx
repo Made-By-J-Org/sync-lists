26fabf03cafa27d2dd81c4275a8f766e374f5d83
juliusz-jakubowski <juliusz.jakubowski@codete.com>
2023-11-01 13:56:11 +0100

    check data before rendering

--- src/components/Lists/Lists.tsx
+++ src/components/Lists/Lists.tsx
@@ -11,15 +11,13 @@ import Loader from "../Loader/Loader";
 import styles from './Lists.module.scss';
 
 const Lists = (): React.ReactElement => {
-
-  // initial load
   const data = useAtomValue(loadableFetchDataAsyncAtom)
   if (data.state === 'hasError') return <div>{typeof data.error === 'string' ? data.error : 'Sorry, something went wrong'}</div>
   if (data.state === 'loading') return <Loader message="One more sec..."/>
 
   return (
     <div className={styles.listsWrapper}>
-      {data?.data.map((item, i) =>
+      {data?.data && data?.data.map((item, i) =>
         <Collapsable key={i} tasksList={item}/>
       )}
     </div>
