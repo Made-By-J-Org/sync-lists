b10900f9bb97ff58ea4085c8352d5fa0c9e65a30
juliusz-jakubowski <juliusz.jakubowski@codete.com>
2023-11-01 14:08:32 +0100

    resolve build fail

--- src/components/Task/Task.spec.tsx
+++ src/components/Task/Task.spec.tsx
@@ -7,27 +7,28 @@ describe("Task component", () => {
   const task = {
     checked: false,
     description: "Sample task",
+    id: '1',
     value: 10
   };
 
   it('render component', () => {
-    const {baseElement} = render(<Task task={task}/>);
+    const {baseElement} = render(<Task tasksListId={'test'} task={task}/>);
     expect(baseElement).toBeInTheDocument();
   });
 
   it('match snapshot', () => {
-    const {asFragment} = render(<Task task={task}/>);
+    const {asFragment} = render(<Task tasksListId={'test'} task={task}/>);
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("should render the task description", () => {
-    const {getByText} = render(<Task task={task}/>);
+    const {getByText} = render(<Task tasksListId={'test'} task={task}/>);
     expect(getByText("Sample task")).toBeInTheDocument();
   });
 
   it("should toggle the selected state on click", async () => {
     const user = userEvent.setup()
-    const {getByTestId} = render(<Task task={task}/>);
+    const {getByTestId} = render(<Task tasksListId={'test'} task={task}/>);
     const taskWrapperCheckbox = getByTestId("task-wrapper-checkbox");
 
     expect(taskWrapperCheckbox.getAttribute('class')).not.toMatch(/checkboxSelected/gi)
