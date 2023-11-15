d12d3d4dd9322da8f3b17ec217c985b177d1efd6
juliusz jakubowski <juliusz.jakubowski@noreply.github.com>
2023-11-15 17:12:32 +0100

    minor change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1>Test</h1>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
