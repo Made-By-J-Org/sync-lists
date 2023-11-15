41e711f3682ac59756108fd0030289b2122c81e5
ingaluczaj <inga.luczaj@noreply.github.com>
2023-11-15 17:19:30 +0100

    1st commit

--- src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const API_ENDPOINT = `https://gist.githubusercontent.com/huvber/ba0d534f68e34f1be86d7fe7eff92c96/raw/98a91477905ea518222a6d88dd8b475328a632d3/mock-progress`
--- src/setupTest.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import matchers from '@testing-library/jest-dom/matchers';
-import {afterEach, expect} from 'vitest';
-import {cleanup} from '@testing-library/react';
-
-expect.extend(matchers);
-afterEach(() => {
-  cleanup();
-});
