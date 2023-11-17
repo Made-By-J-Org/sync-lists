8162ac9da2a7309d8b0c5b089132b84157a07b73
juliusz jakubowski <juliusz.jakubowski@noreply.github.com>
2023-11-17 14:55:38 +0100

    G_01 small help useState

--- src/App.js
+++ src/App.js
@@ -1,75 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import { getValue } from '@testing-library/user-event/dist/utils';
-import { useRef } from "react";
-export default App;
 
 
 function App() {
+   const [userData, setUserData] = useState({
+       name: '',
+       surname: ''
+   })
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-  // don't know what does "e" stands for and "\n"
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const first = e.target.fname.value;
-    const last = e.target.lname.value;
-    console.log("First name :" + first, "\n", "Last name: " + last);
-  };
+const handleSetUserData = (e) => {
+    setUserData(
+        {
+        ...userData,
+        test: e.target.value
+    }
+    )
+   }
 
-  // dont know why isn't working line:41
-  // const App = () => {
-  //   const inputRef = useRef(null);
-
-  //   function handleClick() {
-  //     console.log(inputRef.current.value);
-  //   };
-  // };
-
+  const buttonClick = () => {
+      console.log(userData)
+  }
 
   return (
     <React.Fragment>
-      <button>TestButton</button>
+        <input
+            type="text"
+            placeholder="First name"
+            name="name"
+            onChange={handleSetUserData}
+        />
+        <input
+            type="text"
+            placeholder="Second name"
+            name="surname"
+            onChange={handleSetUserData}
+        />
+          {error && <div>error name is to short</div>}
 
-      {/* Used button version */}
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="fname" placeholder="First name" /><br></br>
-        <input type="text" name="lname" placeholder="Last name" /><br></br>
-        <button>Submit</button>
-      </form>
-
-
-      {/* Don't know why this isn't working */}
-      {/* 
-      <div>
-      <input
-        ref={inputRef}
-        type="text"
-        id="message"
-        name="message"
-      />
-
-      <button onClick={handleClick}>Log message</button>
-    </div> */}
-
-      <div className="App">
-        <div class="testp">
-          <p>test</p>
-        </div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <br/>
+          <br/>
+        <button onClick={buttonClick}>Submit</button>
     </React.Fragment>
   );
 };
+
+export default App;
