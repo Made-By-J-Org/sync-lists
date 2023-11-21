e64a8dd1716e25a1834c06eeee71620d46a7132c
August Jakubowski <jakubowski.august@gmail.com>
2023-11-21 19:34:17 +0100

    Form first version

--- src/App.js
+++ src/App.js
@@ -1,161 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import parsePhoneNumber, { PhoneNumberMatcher } from 'libphonenumber-js';
+import {
+  isPossiblePhoneNumber,
+  isValidPhoneNumber,
+  validatePhoneNumberLength
+} from 'libphonenumber-js';
 export default App;
 
 function App() {
+  const emailRegex = new RegExp(
+    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$');
 
-  const inputFields = ["text", "name", "text", "susrname"];
-  const [isInputDisable, setIsInputDisable] = useState(true);
+
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
+  const [gender, setGender] = useState('');
+  const [email, setEmail] = useState('');
 
 
-  // const inputFieldsGenerator = () => {
-  //   return inputFields.map((e) => (
-  //     <input name={e} />
-  //   ))
-
-  //   // return (<input type={inputFields[0]} name={inputFields[1]} />)
-  // }
-  // const myInputs = (inputType, inputName, inputOnChange, inputClassName) => {
-  //   return <input type={inputType} name={inputName} onChange={inputOnChange} className={inputClassName} />
-  // }
-
-  const validateName = (e) => {
-
-    if (e.length >= 5) {
-      setIsInputDisable(false);
+  const validateEmail = (e) => {
+    const valueForEmail = (e.target.value);
+    setEmail(valueForEmail)
+    console.log("1", emailRegex.test(valueForEmail))
+    if (emailRegex.test(valueForEmail)) {
+      console.log("2", email, valueForEmail,)
+    } else {
+      console.log("change an email")
     }
-    setName(e)
+
   }
 
   const handleSubmit = () => {
     if (name.length >= 5) {
-      console.log(name, surname)
+      console.log(gender, name, surname, email)
     } else {
-      console.log("warrning")
+      console.log("warrning one or more fields needs interaction")
     };
   };
 
-
-
-
-  // const [email, setEmail] = useState('');
-  // const [gender, setGender] = useState('');
-  // const [sure, setSure] = useState('');
-
-
-  // Button tests
-  // const handleButtonClick = (e) => {
-
+  // const phoneNumber = parsePhoneNumber(' 8 (800) 555-35-35 ', 'RU')
+  // // if (phoneNumber) {
+  // //   phoneNumber.country === 'RU'
+  // //   phoneNumber.number === '+78005553535'
+  // //   phoneNumber.isPossible() === true
+  // //   phoneNumber.isValid() === true
+  // //   // Note: `.getType()` requires `/max` metadata: see below for an explanation.
+  // //   phoneNumber.getType() === 'TOLL_FREE'
   // }
+  // isPossiblePhoneNumber('8 (800) 555-35-35', 'RU') === true
+  // isValidPhoneNumber('8 (800) 555-35-35', 'RU') === true
 
-  // const handleNameChange = (e) => {
-  //   setName(e.target.value);
-  // }
-
-  // const handleEmailChange = (e) => {
-  //   setEmail(e.target.value);
-  // }
-
-  // const handleGenderChange = (e) => {
-  //   setGender(e.target.value);
-  // }
-  // const handleSureChange = (e) => {
-  //   setSure(e.target.value);
-  // }
-
-  // // const radioValues = [
-  // //   { value: male, checked: false},
-  // //   { value: female, checked: false},
-  // //   { value: other, checked: false}
-  // //   ]
-
-  // const handleButtonClick = (e) => {
-  //   // e.preventDefault();
-  //   console.log('Name:', name);
-  //   console.log('Email:', email);
-  //   console.log('Gender:', gender);
-  //   console.log('Sure', sure);
-  // }
-  //   function getDate() {
-  //     const today = new Date();
-  //     const month = today.getMonth() + 1;
-  //     const year = today.getFullYear();
-  //     const date = today.getDate();
-  //     return `${month}/${date}/${year}`;
-  //   } 
-  //        const [currentDate, setCurrentDate] = useState(getDate());
-
-
+  // validatePhoneNumberLength('8 (800) 555', 'RU') === 'TOO_SHORT'
+  // validatePhoneNumberLength('8 (800) 555-35-35', 'RU') === undefined // Length is valid.
 
   return (
     <React.Fragment>
 
-
-      {/* <form className="form">
-        <label className="question"><b>
-          Are you ready for the dream work?</b>
+      <div className='form'>
+        <input type="radio" name="gender" value="male" onChange={(e) => setGender(e.target.value)} required={true} />Mr
+        <input type="radio" name="gender" value="female" onChange={(e) => setGender(e.target.value)} required={true} />Mrs
+        <br></br>
+        <label>
+          First Name
+          <br></br>
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)} />
         </label>
-        <input type="checkbox" name="sure" checked={sure === 'yes'} value="yes" onChange={handleSureChange} />Yes <br></br>
-        <input type="checkbox" name="sure" checked={sure === 'no'} value="no" onChange={handleSureChange} />No <br></br>
-        <input type="text" name="name" placeholder='Your name' value={name} onChange={handleNameChange} />
         <br />
-        <input type="email" name="email" placeholder="Your email" value={email} onChange={handleEmailChange} />
-        <br />
-        <input type="radio" value="male" checked={gender === 'male'} onChange={handleGenderChange} />Male
-        <input type="radio" value="female" checked={gender === 'female'} onChange={handleGenderChange} />Female
-        <br />
-        <button type="button" onClick={handleButtonClick}>LETS DO IT</button>
-        {/* <button type="submit">Submit</button> */}
-      {/* </form>  */}
-      {/* second form where i used disabled */}
+        <label className='preview'>Surname<br></br>
+          <input type="text" name="surname" onChange={(e) => setSurname(e.target.value)} required={true} />
 
-
-      <div>
-        <input
-          type="text"
-          onChange={(e) => validateName(e.target.value)} />
-        <br />
-        <input type="text" name="surname" onChange={(e) => setSurname(e.target.value)} required={true} disabled={isInputDisable} />
+        </label> <br></br>
+        <lable>Your email<br></br>
+          <input type="email" name="email" onChange={(e) => validateEmail(e)} required={true}></input>
+        </lable><br></br>
+        {/* <lable>Phone number<br></br>
+          <input type="number" name="phone" onClick={parsePhoneNumber} ></input></lable><br></br> */}
         <button type="button" onClick={handleSubmit}>
           Submit
         </button>
-        {/* {myInputs("text", "number", handleSubmit, "inputClassName")}
-        {inputFieldsGenerator()} */}
+
       </div>
 
 
 
 
 
-
-
-
-
-
-
-
-      <div className="App">
-        <div className="test">
-          <p></p>
-        </div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
     </React.Fragment>
   );
 };
